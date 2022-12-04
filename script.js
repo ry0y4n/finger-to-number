3126a9a58ed62604e5945cd3935edd8583c29e03
momosuke <rusk.ryoy4n@gmail.com>
2022-12-04 19:08:00 +0900

    remove ignored files

--- script.js
+++ script.js
@@ -12,19 +12,8 @@ function onResults(results) {
     canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
     if (results.multiHandLandmarks) {
         for (const landmarks of results.multiHandLandmarks) {
-            // for (const handedness of results.multiHandedness) {
-            //     if (handedness.label == "Left") {
-            //         console.log("Right Hand")
-
-            //     }
-            //     else {
-            //         console.log("Left Hand")
-            //     }
-            // }
-            
             drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {color: '#00FF00', lineWidth: 5});
             drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
-            // console.log(landmarks)
         }
         for (const worldLandmarks of results.multiHandWorldLandmarks) {
             for (const handedness of results.multiHandedness) {
@@ -32,7 +21,6 @@ function onResults(results) {
                 let num = 0
                 let fingers = ["thumb", "index", "middle", "ring", "little"]
 
-                // 右手の処理
                 if (handedness.label == "Left") {
                     console.log("Right Hand")
 
@@ -49,11 +37,6 @@ function onResults(results) {
                     canvasCtx.font = "120px serif"
                     canvasCtx.fillStyle = "white"
                     canvasCtx.fillText(num, 70, 160)
-                }
-                else {
-                    console.log("Left Hand")
-                    let rad = ((worldLandmarks[9].x - worldLandmarks[10].x)*(worldLandmarks[12].x - worldLandmarks[10].x) + (worldLandmarks[9].y - worldLandmarks[10].y)*(worldLandmarks[12].y - worldLandmarks[10].y) + (worldLandmarks[9].z - worldLandmarks[10].z)*(worldLandmarks[12].z - worldLandmarks[10].z)) / (Math.sqrt((worldLandmarks[9].x - worldLandmarks[10].x)**2 + (worldLandmarks[9].y - worldLandmarks[10].y)**2 + (worldLandmarks[9].z - worldLandmarks[10].z)**2) * Math.sqrt((worldLandmarks[12].x - worldLandmarks[10].x)**2 + (worldLandmarks[12].y - worldLandmarks[10].y)**2 + (worldLandmarks[12].z - worldLandmarks[10].z) **2));
-                    console.log(rad)
                 }
             }
         }
